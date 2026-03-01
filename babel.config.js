fe27dde0bc1f595d598852c49fc45f9a20faa8f5
Soham407 <sohambhutkar1@gmail.com>
2026-03-01 14:41:58 +0530

    Fix Reanimated configuration

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'] // Remove the plugins array completely
+    presets: ['babel-preset-expo']
   };
 };
